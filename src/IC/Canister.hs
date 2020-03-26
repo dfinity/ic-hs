2b5a9c2d52ade13827455d91cfeccfcf10a171bf
Joachim Breitner <mail@joachim-breitner.de>
2020-03-26 18:24:17 +0100

    stash
    Originally https://github.com/dfinity-lab/ic-ref/commit/17d409a8eec55b3099a5aa63dfac83f4ea98fc29

--- src/IC/Canister.hs
+++ src/IC/Canister.hs
@@ -20,11 +20,13 @@ import qualified Data.Map as M
 import Data.List
 
 import IC.Types
-import IC.Wasm.Winter (parseModule, exportedFunctions, Module)
+--import IC.Wasm.Winter (parseModule, exportedFunctions, Module)
 
-import qualified IC.Canister.Interface as CI
 -- Here we can swap out the persistence implementation
-import IC.Canister.Persisted
+-- import qualified IC.Canister.Interface as CI
+-- import IC.Canister.Persisted
+
+data WasmState = WasmState deriving Show
 
 type InitFunc = CanisterId -> EntityId -> Blob -> TrapOr WasmState
 type UpdateFunc = WasmState -> TrapOr (WasmState, UpdateResult)
@@ -43,34 +45,7 @@ instance Show CanisterModule where
     show _ = "CanisterModule{...}"
 
 parseCanister :: Blob -> Either String CanisterModule
-parseCanister bytes =
-  case parseModule bytes of
-    Left  err -> Left err
-    Right wasm_mod -> Right $ concreteToAbstractModule wasm_mod
-
-concreteToAbstractModule :: Module -> CanisterModule
-concreteToAbstractModule wasm_mod = CanisterModule
-  { init_method = \cid caller dat -> initialize wasm_mod cid caller dat
-  , update_methods = M.fromList
-    [ (m,
-      \caller responded dat wasm_state ->
-      invoke wasm_state (CI.Update m caller responded dat))
-    | n <- exportedFunctions wasm_mod
-    , Just m <- return $ stripPrefix "canister_update " n
-    ]
-  , query_methods = M.fromList
-    [ (m, \caller arg wasm_state ->
-        snd <$> invoke wasm_state (CI.Query m caller arg))
-    | n <- exportedFunctions wasm_mod
-    , Just m <- return $ stripPrefix "canister_query " n
-    ]
-  , callbacks = \cb responded res wasm_state ->
-    invoke wasm_state (CI.Callback cb responded res)
-  , pre_upgrade_method = \wasm_state caller ->
-        snd <$> invoke wasm_state (CI.PreUpgrade wasm_mod caller)
-  , post_upgrade_method = \cid caller mem dat ->
-        initializeUpgrade wasm_mod cid caller mem dat
-  }
+parseCanister bytes = undefined
 
 -- | Turns a query function into an update function
 asUpdate ::
