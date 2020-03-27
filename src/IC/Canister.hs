f8390159e17dfa9f2dc3f5f9dbcf9715acaed1c7
Joachim Breitner <mail@joachim-breitner.de>
2020-03-27 14:52:37 +0100

    Use CPP to select Canister implementation
    Originally https://github.com/dfinity-lab/ic-ref/commit/fe2888c3af66dcbc6c69839e1cfe794db2886c4c

--- src/IC/Canister.hs
+++ src/IC/Canister.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE CPP #-}
 
 {-|
 A “pure” implementation of canisters, using "IC.Canister.Imp", but just replaying when needed.
@@ -20,13 +21,18 @@ import qualified Data.Map as M
 import Data.List
 
 import IC.Types
---import IC.Wasm.Winter (parseModule, exportedFunctions, Module)
 
--- Here we can swap out the persistence implementation
--- import qualified IC.Canister.Interface as CI
--- import IC.Canister.Persisted
+#if defined(CANISTER_PURE) || defined(CANISTER_PERSISTED)
+import IC.Wasm.Winter (parseModule, exportedFunctions, Module)
+import qualified IC.Canister.Interface as CI
+#endif
 
-data WasmState = WasmState deriving Show
+#if defined(CANISTER_PURE)
+import IC.Canister.Pure
+#endif
+#if defined(CANISTER_PERSISTED)
+import IC.Canister.Persisted
+#endif
 
 type InitFunc = CanisterId -> EntityId -> Blob -> TrapOr WasmState
 type UpdateFunc = WasmState -> TrapOr (WasmState, UpdateResult)
@@ -41,12 +47,6 @@ data CanisterModule = CanisterModule
   , post_upgrade_method :: CanisterId -> EntityId -> Blob -> Blob -> TrapOr WasmState
   }
 
-instance Show CanisterModule where
-    show _ = "CanisterModule{...}"
-
-parseCanister :: Blob -> Either String CanisterModule
-parseCanister bytes = undefined
-
 -- | Turns a query function into an update function
 asUpdate ::
   (EntityId -> Blob -> QueryFunc) ->
@@ -56,3 +56,50 @@ asUpdate f caller (Responded responded) dat wasm_state
   | otherwise =
     (\res -> (wasm_state, ([], Just res))) <$>
     f caller dat wasm_state
+
+instance Show CanisterModule where
+    show _ = "CanisterModule{...}"
+
+
+#if defined(CANISTER_PURE) || defined(CANISTER_PERSISTED)
+parseCanister :: Blob -> Either String CanisterModule
+parseCanister bytes =
+  case parseModule bytes of
+    Left  err -> Left err
+    Right wasm_mod -> Right $ concreteToAbstractModule wasm_mod
+
+concreteToAbstractModule :: Module -> CanisterModule
+concreteToAbstractModule wasm_mod = CanisterModule
+  { init_method = \cid caller dat -> initialize wasm_mod cid caller dat
+  , update_methods = M.fromList
+    [ (m,
+      \caller responded dat wasm_state ->
+      invoke wasm_state (CI.Update m caller responded dat))
+    | n <- exportedFunctions wasm_mod
+    , Just m <- return $ stripPrefix "canister_update " n
+    ]
+  , query_methods = M.fromList
+    [ (m, \caller arg wasm_state ->
+        snd <$> invoke wasm_state (CI.Query m caller arg))
+    | n <- exportedFunctions wasm_mod
+    , Just m <- return $ stripPrefix "canister_query " n
+    ]
+  , callbacks = \cb responded res wasm_state ->
+    invoke wasm_state (CI.Callback cb responded res)
+  , pre_upgrade_method = \wasm_state caller ->
+        snd <$> invoke wasm_state (CI.PreUpgrade wasm_mod caller)
+  , post_upgrade_method = \cid caller mem dat ->
+        initializeUpgrade wasm_mod cid caller mem dat
+  }
+
+#endif
+
+#if defined(CANISTER_FAKE)
+-- this is so that IC.Ref can be loaded into hs-to-coq without pulling in all
+-- of winter
+
+data WasmState = WasmState deriving Show
+
+parseCanister :: Blob -> Either String CanisterModule
+parseCanister bytes = undefined
+#endif
