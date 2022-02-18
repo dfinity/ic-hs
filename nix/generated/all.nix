4a027c6b969a025a94ce582e8897c2ce80a4b313
Marcin Dziadus <84316545+marcin-dziadus@users.noreply.github.com>
2022-02-18 17:21:57 +0100

    Bump http-client (#73)
    * Bump version of http-client
    
    * nix: package http-client-0.7.11
    
    * Fix cabal.freeze
    
    * Simplify
    
    * Revert "Simplify"
    
    This reverts commit a4854c4a6572ca70f653d74a812c1069f03ffa63.
    
    * Move definition
    
    * Fix typo
    
    Co-authored-by: Bas van Dijk <bas@van.dijk.ch>

--- nix/generated/all.nix
+++ nix/generated/all.nix
@@ -1,6 +1,7 @@
 self: super: {
   candid = super.callPackage ./candid.nix { };
   cborg = super.callPackage ./cborg.nix { };
+  http-client = super.callPackage ./http-client.nix { };
   ic-hs = super.callPackage ./ic-hs.nix { };
   leb128-cereal = super.callPackage ./leb128-cereal.nix { };
   winter = super.callPackage ./winter.nix { };
