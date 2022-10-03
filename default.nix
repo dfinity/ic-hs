2af3b2bb713b82e933cebe636cdf118e08abecbc
Martin Raszyk <martin.raszyk@dfinity.org>
2022-10-03 18:45:24 +0200

    point to local httpbin server

--- default.nix
+++ default.nix
@@ -160,7 +160,7 @@ rec {
       test -e port
       mkdir -p $out
       ${httpbin} &
-      LANG=C.UTF8 ic-ref-test --endpoint "http://0.0.0.0:$(cat port)/" --html $out/report.html
+      LANG=C.UTF8 ic-ref-test --endpoint "http://0.0.0.0:$(cat port)/" --httpbin "http://0.0.0.0:8003" --html $out/report.html
 
       mkdir -p $out/nix-support
       echo "report test-results $out report.html" >> $out/nix-support/hydra-build-products
@@ -178,7 +178,7 @@ rec {
       sleep 1
       test -e port
       ${httpbin} &
-      LANG=C.UTF8 ic-ref-test --endpoint "http://0.0.0.0:$(cat port)/"
+      LANG=C.UTF8 ic-ref-test --endpoint "http://0.0.0.0:$(cat port)/" --httpbin "http://0.0.0.0:8003"
       kill -INT %1
       trap - EXIT PIPE
       sleep 5 # wait for ic-ref.tix to be written
