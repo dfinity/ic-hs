1500e30f0c540bf1b02de15d3d6a07ceb396e355
Martin Raszyk <martin.raszyk@dfinity.org>
2023-04-11 08:03:50 +0200

    drop one unused dep

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -2,7 +2,6 @@ pub mod active_execution_state_registry;
 pub mod controller_service_impl;
 pub mod launch_as_process;
 mod process_exe_and_args;
-pub mod process_os_metrics;
 mod sandbox_process_eviction;
 pub mod sandboxed_execution_controller;
 
