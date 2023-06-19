ff93d073e5ca72d2e6aeea058f035cdda6b08b6e
Martin Raszyk <martin.raszyk@dfinity.org>
2023-06-19 21:09:36 +0200

    add one more test

--- src/IC/Test/Spec/CanisterHistory.hs
+++ src/IC/Test/Spec/CanisterHistory.hs
@@ -60,7 +60,7 @@ canister_history_tests ecid =
       info <- get_canister_info unican cid (Just 1)
       void $ check_history info 2 [(1, ChangeFromUser (EntityId defaultUser), CodeDeployment Install (sha256 universal_wasm))]
 
-      ic_install_with_sender_canister_version ic00 (enum #reinstall) cid trivialWasmModule "" (Just 666)
+      ic_install_with_sender_canister_version ic00 (enum #reinstall) cid trivialWasmModule "" (Just 666) -- sender_canister_version in ingress message is ignored
       info <- get_canister_info unican cid (Just 1)
       void $ check_history info 3 [(2, ChangeFromUser (EntityId defaultUser), CodeDeployment Reinstall (sha256 trivialWasmModule))]
 
@@ -96,7 +96,8 @@ canister_history_tests ecid =
       void $ forM [1..24] $ \i -> do
         ic_set_controllers ic00 cid [defaultUser]
         info <- get_canister_info unican cid (Just 20)
-        let hist = reverse $ take 20 $ reverse $ (0, ChangeFromUser (EntityId defaultUser), Creation [(EntityId defaultUser)]) : map (\i -> (i, ChangeFromUser (EntityId defaultUser), ControllersChange [EntityId defaultUser])) [1..i]
+        let hist = reverse $ take 20 $ reverse $ (0, ChangeFromUser (EntityId defaultUser), Creation [(EntityId defaultUser)]) :
+              map (\i -> (i, ChangeFromUser (EntityId defaultUser), ControllersChange [EntityId defaultUser])) [1..i]
         void $ check_history info (i + 1) hist
 
       return ()
@@ -113,6 +114,15 @@ canister_history_tests ecid =
       void $ check_history info 2 [(1, ChangeFromCanister (EntityId unican) (Just 5), CodeDeployment Install (sha256 trivialWasmModule))]
 
       return ()
+
+    , simpleTestCase "does not track all update_settings calls" ecid $ \unican -> do
+      cid <- ic_provisional_create ic00 ecid Nothing Nothing R.empty
+      ic_update_settings ic00 cid (R.empty .+ #freezing_threshold .== 2^(10::Int)) -- not stored in canister history; canister version still bumped
+      ic_install ic00 (enum #install) cid trivialWasmModule ""
+
+      info <- get_canister_info unican cid (Just 2)
+      void $ check_history info 2 [(0, ChangeFromUser (EntityId defaultUser), Creation [(EntityId defaultUser)]),
+        (2, ChangeFromUser (EntityId defaultUser), CodeDeployment Install (sha256 trivialWasmModule))]
 
     , testCase "incorrect sender_canister_version" $ do
       unican <- install ecid no_heartbeat
@@ -146,7 +156,12 @@ canister_history_tests ecid =
       info .! #controllers @?= (Vec.fromList [Principal otherUser, Principal defaultUser])
       info .! #module_hash @?= Nothing
 
-      let hist = [(0, ChangeFromUser (EntityId defaultUser), Creation [(EntityId defaultUser)]), (1, ChangeFromUser (EntityId defaultUser), CodeDeployment Install (sha256 trivialWasmModule)), (2, ChangeFromUser (EntityId defaultUser), CodeDeployment Reinstall (sha256 universal_wasm)), (3, ChangeFromUser (EntityId defaultUser), CodeDeployment Upgrade (sha256 trivialWasmModule)), (4, ChangeFromUser (EntityId defaultUser), CodeUninstall), (5, ChangeFromUser (EntityId defaultUser), ControllersChange [EntityId otherUser, EntityId defaultUser])]
+      let hist = [(0, ChangeFromUser (EntityId defaultUser), Creation [(EntityId defaultUser)]),
+            (1, ChangeFromUser (EntityId defaultUser), CodeDeployment Install (sha256 trivialWasmModule)),
+            (2, ChangeFromUser (EntityId defaultUser), CodeDeployment Reinstall (sha256 universal_wasm)),
+            (3, ChangeFromUser (EntityId defaultUser), CodeDeployment Upgrade (sha256 trivialWasmModule)),
+            (4, ChangeFromUser (EntityId defaultUser), CodeUninstall),
+            (5, ChangeFromUser (EntityId defaultUser), ControllersChange [EntityId otherUser, EntityId defaultUser])]
 
       info <- get_canister_info unican cid (Just 0)
       void $ check_history info 6 []
